bdf490eaa2beb5a7fba400c1316bb06c6c7da3d7
권태확 <taehwak@hanyang.ac.kr>
2020-09-24 01:03:05 +0900

    200924

--- IM대비/2527.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-
-for i in range(4):
-    squares = list(map(int, input().split()))
-    square_a = squares[:4]
-    square_b = squares[4:]
-    w = sq
--- IM대비/BOJ2527.py
+++ IM대비/BOJ2527.py
@@ -1,35 +1,28 @@
 import sys
 sys.stdin = open('input.txt')
 
-def gen_points(square):
-    points = []
-    for i in range(0,4,2):
-        points.append((square[i],square[i+1]))
-        points.append((square[i],square[3-i]))
-    return points
-
-
-
 for i in range(4):
     squares = list(map(int, input().split()))
     square_a = squares[:4]
     square_b = squares[4:]
-    points_a = gen_points(square_a)
-    points_b = gen_points(square_b)
+    left = max(square_a[0],square_b[0])
+    right = min(square_a[2],square_b[2])
+    top = min(square_a[3], square_b[3])
+    bottom = max(square_a[1], square_b[1])
 
-    if (len(set(points_a)&set(points_b)) == 1):
+    width = right - left
+    height = top - bottom
+    
+    if width == 0 and height == 0:
         print('c')
-    elif square_a[2] < square_b[0] and square_a[3] < square_b[1]:
-        print('a')
-    elif (square_a[0] > square_b[2] or square_a[1] > square_b[3] or square_a[2] < square_b[0] or square_a[3] < square_b[
-        1]):
-        print('d')
-    elif (square_b[0] > square_a[2] or square_b[1] > square_a[3] or square_b[2] < square_a[0] or square_b[3] < square_a[
-        1]):
-        print('d')
-
-    else:
+    elif width == 0 or height == 0:
         print('b')
+    elif width < 0 or height < 0:
+        print('d')
+    else:
+        print('a')
+
+
 '''
 a
 a in b or b in a
